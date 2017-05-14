fcad767dcd23a197ced4514c08e5d849e6baeef1
James LaChance <fatboyxpc@gmail.com>
2017-05-14 14:49:14 -0400

    Changed the testing to use DB Transactions Instructions followed from Adam Wathan's [blog post](https://adamwathan.me/2016/11/14/a-better-database-testing-workflow-in-laravel/).

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -7,4 +7,11 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+    use DatabaseSetup;
+
+    protected function setUp()
+    {
+        parent::setUp();
+        $this->setupDatabase();
+    }
 }
